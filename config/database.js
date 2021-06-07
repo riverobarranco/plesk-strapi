ca9c0c2cda4af2b02e1d0d3c2c25b9f367839254
Eduardo Rivero Barranco <riverobarranco@gmail.com>
2021-06-07 23:33:30 +0200

    configuraciones extra

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
+// config/database.js
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: env('DATABASE_CONNECTION_NAME'), 
   connections: {
-    default: {
+    sqlite: {
       connector: 'bookshelf',
       settings: {
         client: 'sqlite',
@@ -11,5 +12,18 @@ module.exports = ({ env }) => ({
         useNullAsDefault: true,
       },
     },
+    mysql: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    }
   },
 });
